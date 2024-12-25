6d4236ddc934c75aa5db5887d6e4d483803a6ba2
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-12-25 01:15:42 -1100

    1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <header className='header'>
+      <NavLink to="/">
+        
+     </NavLink>
+    </header>
   )
 }
 
