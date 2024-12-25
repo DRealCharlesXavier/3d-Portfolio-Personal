3afedeb28d933d974cd4754180716f1e44b6af6e
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-12-25 01:19:45 -1100

    2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-   <header className='header'>
-      <NavLink to="/">
-        
-     </NavLink>
+    <header className="header">
+      <NavLink
+        to="/"
+        className="w-10 h-10 rounded-lgbg-white items-center justify-center flex font-bold shadow-md"
+      >
+        <p className="blue-gradient_text">XC</p>
+      </NavLink>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
