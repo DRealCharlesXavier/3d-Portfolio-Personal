646ab862d417ed9c887a898fec16e69c186c2fc9
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-12-28 11:10:14 -1100

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,17 +11,19 @@ import Island from "../models/Island";
 
 const Home = () => {
   const adjustIslandForScreenSize = () => {
-    let screenScale = null;
-    let screenPosition = { 0, - 6.5, -43
-  };
+    let screenScale = null,
+    let screenPosition = [0, -6.5, -43];
 
-  if (window.innerWidth < 768) {
-    screenScale = [0.9, 0.9, 0.9];
-    screenPosition = [0, -6.5, -43];
-  } else {
-    screenPosition = [0, -6.5, 46]
+    if (windows.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition]
   }
-  }
+
+  const [islandScale, islandPosition] = adjustIslandForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
