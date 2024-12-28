d2a17bc91d126fa9c6653c46b9e62c6ea75fa867
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2024-12-28 11:15:56 -1100

    6

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,8 +11,9 @@ import Island from "../models/Island";
 
 const Home = () => {
   const adjustIslandForScreenSize = () => {
-    let screenScale = null,
+    let screenScale = null;
     let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
 
     if (windows.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9];
@@ -20,28 +21,33 @@ const Home = () => {
       screenScale = [1, 1, 1];
     }
 
-    return [screenScale, screenPosition]
-  }
+    return [screenScale, screenPosition, rotation];
+  };
 
-  const [islandScale, islandPosition] = adjustIslandForScreenSize();
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
-      <Canvas className="w-full h-screen bg-transparent"  
-      camera={{ near: 0.1, far: 1000 }}
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
       >
-        <Suspense fallback={<Loader />}>
+        <Suspense fallback={<Loader />}> 
           <directionalLight />
           <ambientLight />
           <pointLight />
           <spotLight />
           <hemisphereLight />
 
-          <Island />
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
         </Suspense>
       </Canvas>
     </section>
-  )
+  );
 };
 
 export default Home;
