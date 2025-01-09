3ce04a3e1a5eda6022c468760f811a562e6b45b7
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2025-01-08 23:42:53 -1100

    5

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -9,16 +9,44 @@ Title: Fox's islands
 import { useRef, useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
-import { a } from '@react-spring/three'
+import { a } from "@react-spring/three";
 
-import islandScene from "../assets/3d/island.glb"
+import islandScene from "../assets/3d/island.glb";
 
 const Island = ({ isRotating, setIsRotating, ...props }) => {
- const islandRef = useRef();
+  const islandRef = useRef();
 
- const { gl, viewport } = useThree()
- const { nodes, materials } = useGLTF("/island.glb");
- 
+  const { gl, viewport } = useThree();
+  const { nodes, materials } = useGLTF("/island.glb");
+
+  const lastX = useRef(0);
+  const rotationSpeed = useRef(0);
+  const dampingFactor = 0.95;
+
+  const handlePointerDown = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    setIsRotating(true);
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+    lastX.current = clientX;
+  };
+  const handlePointerUp = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    setIsRotating(false);
+  };
+  const handlePointerMove = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+   // setIsRotating(true);
+   
+   const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+   lastX.current = clientX;
+  };
+
   return (
     <a.group ref={islandRef} {...props}>
       <mesh
@@ -33,7 +61,7 @@ const Island = ({ isRotating, setIsRotating, ...props }) => {
         geometry={nodes.polySurface946_tree2_0.geometry}
         material={materials.PaletteMaterial001}
       />
-      <mesh 
+      <mesh
         geometry={nodes.polySurface947_tree1_0.geometry}
         material={materials.PaletteMaterial001}
       />
@@ -51,6 +79,6 @@ const Island = ({ isRotating, setIsRotating, ...props }) => {
       />
     </a.group>
   );
-}
+};
 
 export default Island;
