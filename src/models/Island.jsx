527a410cd3432b0eb2875d8159019e2a7f012349
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2025-01-08 23:52:47 -1100

    6

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -40,11 +40,17 @@ const Island = ({ isRotating, setIsRotating, ...props }) => {
   const handlePointerMove = (e) => {
     e.stopPropagation();
     e.preventDefault();
-   // setIsRotating(true);
-   
-   const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+    // setIsRotating(true);
 
-   lastX.current = clientX;
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+    lastX.current = clientX;
+
+    const delta = (clientX - lastX.current) / viewport.width;
+
+    islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+    lastX.current = clientX;
+    rotationSpeed.current = delta * 0.01 * Math.PI;
   };
 
   return (
