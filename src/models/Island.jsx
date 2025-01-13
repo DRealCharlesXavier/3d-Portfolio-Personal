32eebd8f74b30a0821e7716f020646e858e50a32
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2025-01-13 10:43:14 -1100

    1

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -45,7 +45,6 @@ const Island = ({ isRotating, setIsRotating, setCurrentStage, ...props }) => {
     if (isRotating) {
       const clientX = e.touches ? e.touches[0].clientX : e.clientX;
 
-      lastX.current = clientX;
 
       const delta = (clientX - lastX.current) / viewport.width;
 
@@ -70,6 +69,27 @@ const Island = ({ isRotating, setIsRotating, setCurrentStage, ...props }) => {
       setIsRotating(false);
     }
   };
+
+  
+
+  useEffect(() => {
+    console.log({ isRotating });
+
+    const canvas = gl.domElement;
+    canvas.addEventListener("pointerdown", handlePointerDown);
+    canvas.addEventListener("pointerup", handlePointerUp);
+    canvas.addEventListener("pointermove", handlePointerMove);
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      canvas.removeEventListener("pointerdown", handlePointerDown);
+      canvas.removeEventListener("pointerup", handlePointerUp);
+      canvas.removeEventListener("pointermove", handlePointerMove);
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
 
   useFrame(() => {
     if (!isRotating) {
@@ -122,55 +142,9 @@ const Island = ({ isRotating, setIsRotating, setCurrentStage, ...props }) => {
     }
   });
 
-  useEffect(() => {
-    console.log({ isRotating });
-
-    const canvas = gl.domElement;
-    canvas.addEventListener("pointerdown", handlePointerDown);
-    canvas.addEventListener("pointerup", handlePointerUp);
-    canvas.addEventListener("pointermove", handlePointerMove);
-    document.addEventListener("keydown", handleKeyDown);
-    document.addEventListener("keyup", handleKeyUp);
-
-    return () => {
-      canvas.removeEventListener("pointerdown", handlePointerDown);
-      canvas.removeEventListener("pointerup", handlePointerUp);
-      canvas.removeEventListener("pointermove", handlePointerMove);
-      document.removeEventListener("keydown", handleKeyDown);
-      document.removeEventListener("keyup", handleKeyUp);
-    };
-  }, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
-
   return (
     <a.group ref={islandRef} {...props}>
-      <mesh
-        geometry={nodes.polySurface944_tree_body_0.geometry}
-        material={materials.PaletteMaterial001}
-      />
-      <mesh
-        geometry={nodes.polySurface945_tree1_0.geometry}
-        material={materials.PaletteMaterial001}
-      />
-      <mesh
-        geometry={nodes.polySurface946_tree2_0.geometry}
-        material={materials.PaletteMaterial001}
-      />
-      <mesh
-        geometry={nodes.polySurface947_tree1_0.geometry}
-        material={materials.PaletteMaterial001}
-      />
-      <mesh
-        geometry={nodes.polySurface948_tree_body_0.geometry}
-        material={materials.PaletteMaterial001}
-      />
-      <mesh
-        geometry={nodes.polySurface949_tree_body_0.geometry}
-        material={materials.PaletteMaterial001}
-      />
-      <mesh
-        geometry={nodes.pCube11_rocks1_0.geometry}
-        material={materials.PaletteMaterial001}
-      />
+     
     </a.group>
   );
 };
