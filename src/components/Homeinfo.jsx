4fa905f4ce2c7eaa18af74a9aacf9f18c8cb113e
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2025-01-18 18:35:40 -1100

    1

--- src/components/Homeinfo.jsx
+++ src/components/Homeinfo.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const renderContent = {
- 1: (
-  <h1>1</h1>
- ),
- 2: (
-  <h1>2</h1>
- ),
- 3: (
-  <h1>3</h1>
- ),
- 4: (
-  <h1>4</h1>
- ),
-}
+  1: (
+    <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
+   Hi, I am <span className="font-semibold">CHARLES XAVIER</span>👋 <br />
+   A Software Engineer from Nigeria
+    </h1>
+  ),
+  2: <h1>2</h1>,
+  3: <h1>3</h1>,
+  4: <h1>4</h1>,
+};
 
-const HomeInfo = () => {
-  return (
-    <div>HomeInfo</div>
-  )
-}
+const InfoBox = ({ text, link, btnText }) => (
+ <div>
+  {text}
+ </div>)
 
-export default HomeInfo
+const HomeInfo = ({ currentStage }) => {
+  return renderContent[currentStage] || null;
+};
+
+export default HomeInfo;
