c59a61b7b5f75c06a690113dccc9bd768ad16e7c
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2025-02-05 03:50:03 -1100

    1

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
+  const [form, setForm] = useState({name: "", email: "", message: "" })
+
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
-      
-    </section>
-  )
-}
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in Touch</h1>
 
-export default Contact
+        <form className="w-full flex flex-col gap-7mt-14">
+          <label className="text-black-500 font-semibold">
+            Name
+            <input type="text" name="name" className="input" placeholder="John Doe" required value={} />
+          </label>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
