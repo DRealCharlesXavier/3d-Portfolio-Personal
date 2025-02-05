f7b97555f4b29a7c707331b31de6c9809e26416d
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2025-02-05 06:18:06 -1100

    3

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,10 +2,11 @@ import React, { useState } from "react";
 
 const Contact = () => {
   const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = () => {};
   const handleFocus = () => {};
-  const handleBlur = () => {}
+  const handleBlur = () => {};
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
@@ -27,6 +28,41 @@ const Contact = () => {
               onBlur={handleBlur}
             />
           </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="Email"
+              name="Email"
+              className="input"
+              placeholder="John@gmail.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Your Message
+            <textarea
+              name="message"
+              rows={4}
+              className="textarea"
+              placeholder="Let me know how I can help you"
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <button
+            type="submit"
+            className="btn"
+            disabled={isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          ></button>
         </form>
       </div>
     </section>
