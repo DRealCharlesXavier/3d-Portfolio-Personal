34e5929f73a5dc696ece92a2ccdfb707045cc15b
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2025-02-05 20:47:03 -1100

    5

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from "react";
-import emailjs from '@emailjs/browser'
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const formRef = useRef(null);
@@ -7,47 +7,55 @@ const Contact = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value })
+    setForm({ ...form, [e.target.name]: e.target.value });
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    setIsLoading(true)
+    setIsLoading(true);
 
-    emailjs.sendForm(
-      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-      {
-        from_name: form.name,
-        to_name: "Xavier",
-        from_email: form.email,
-        to_email: "drealcharlesxavier@proton.me",
-        message: form.message
-      },
-      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-    ).then(() => {
-      setIsLoading(false);
-      // TODO: Show success message
-      // TODO: Hide an alert
-    }).catch((error) => {
-      setIsLoading(false);
-      console.log(error);
-      // TODO: Show error message
-    })
-  }
+    console.log(import.meta.env.VITE_APP_EMAILJS_SERVICE_ID);
+    
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Xavier",
+          from_email: form.email,
+          to_email: "drealcharlesxavier@proton.me",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setIsLoading(false);
+        // TODO: Show success message
+        // TODO: Hide an alert
+
+        setForm({ name: "", email: "", message: "" });
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.log(error);
+        // TODO: Show error message
+      });
+  };
   const handleFocus = () => {};
-  const handleBlur = () => { };
+  const handleBlur = () => {};
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
 
-        <form className="w-full flex flex-col gap-7mt-14"
-        onSubmit={handleSubmit}
+        <form
+          className="w-full flex flex-col gap-7mt-14"
+          onSubmit={handleSubmit}
         >
           <label className="text-black-500 font-semibold">
             Name
-            <input 
+            <input
               type="text"
               name="name"
               className="input"
