da19578202c4590038fb037ad8d173b6b9840063
drealcharlesxavier@proton.me <DRealCharlesXavier@proton.me>
2025-03-03 12:16:34 -1100

    Fixing bugs

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Provider } from "./components/ui/provider";
-import { ColorModeScript } from "@chakra-ui/react";
-import { theme } from "./theme";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </Provider>
 );
